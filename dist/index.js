9fe54f621cb15ee7b62dbabf8bfc025581bd12ec
Darren Corbett <corbsoft@gmail.com>
2016-11-17 11:13:11 +0000

    Exposed webdriver By and until functionality

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,13 @@
 const buildYadda = require('./lib/yadda-core');
 const yaddaLibraryBuilder = require('./lib/yadda-library-builder');
+const webdriver = require('selenium-webdriver');
+
+const By = webdriver.By;
+const until = webdriver.until;
 
 module.exports = {
   yaddaCore: buildYadda,
   yaddaLibrary: yaddaLibraryBuilder,
+  By,
+  until,
 };
