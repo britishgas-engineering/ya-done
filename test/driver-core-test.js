6f344d411c9fcee4876cb5feea2e161bf320ca7c
BIPLAVGHOSAL <BIPLAV.GHOSAL@britishgas.co.uk>
2020-05-28 11:24:22 +0100

    test chrome

--- test/driver-core-test.js
+++ test/driver-core-test.js
@@ -1,37 +1,37 @@
-// import chai from 'chai';
-// import { describe, it, afterEach } from 'mocha';
-// import innerDriverCore from '../dist/lib/driver-core';
+import chai from 'chai';
+import { describe, it, afterEach } from 'mocha';
+import innerDriverCore from '../dist/lib/driver-core';
 
-// const should = chai.should();
-// let driverCore = null;
+const should = chai.should();
+let driverCore = null;
 
-// describe('driver-core :', () => {
-//   afterEach(() => {
-//     driverCore = null;
-//   });
+describe('driver-core :', () => {
+  afterEach(() => {
+    driverCore = null;
+  });
 
-//   describe('returns expected configured driver library', () => {
-//     it('validate the driver core constructor name', (done) => {
-//       driverCore = innerDriverCore({
-//         useBrowser: true,
-//         capabilities: {
-//           browserName: 'chrome',
-//           resolution: '1024x768',
-//           args: [
-//             '--headless',
-//             '--foo',
-//             '--bar'
-//           ]
-//         }
-//       });
-//       should.equal(
-// 				driverCore.constructor.name,
-// 				'thenableWebDriverProxy',
-// 				'library should be a thenableWebDriverProxy function'
-//             );
-//       done();
-//     });
-//   });
+  describe('returns expected configured driver library', () => {
+    it('validate the driver core constructor name', (done) => {
+      driverCore = innerDriverCore({
+        useBrowser: true,
+        capabilities: {
+          browserName: 'chrome',
+          resolution: '1024x768',
+          args: [
+            '--headless',
+            '--foo',
+            '--bar'
+          ]
+        }
+      });
+      should.equal(
+				driverCore.constructor.name,
+				'thenableWebDriverProxy',
+				'library should be a thenableWebDriverProxy function'
+            );
+      done();
+    });
+  });
 
 // describe('framework is object with useBrowser framework prop is used', () => {
 //     it ('triggers the local browser on passing useBrowser attributes for chrome', (done) => {
@@ -80,4 +80,4 @@
 //       })
 //   })
   
-// });
+});
