f0742d392296f4fe391e20fd78166e8538bdbc04
BIPLAVGHOSAL <BIPLAV.GHOSAL@britishgas.co.uk>
2020-05-28 11:20:51 +0100

    test

--- test/driver-core-test.js
+++ test/driver-core-test.js
@@ -1,83 +1,83 @@
-import chai from 'chai';
-import { describe, it, afterEach } from 'mocha';
-import innerDriverCore from '../dist/lib/driver-core';
+// import chai from 'chai';
+// import { describe, it, afterEach } from 'mocha';
+// import innerDriverCore from '../dist/lib/driver-core';
 
-const should = chai.should();
-let driverCore = null;
+// const should = chai.should();
+// let driverCore = null;
 
-describe('driver-core :', () => {
-  afterEach(() => {
-    driverCore = null;
-  });
+// describe('driver-core :', () => {
+//   afterEach(() => {
+//     driverCore = null;
+//   });
 
-  describe('returns expected configured driver library', () => {
-    it('validate the driver core constructor name', (done) => {
-      driverCore = innerDriverCore({
-        useBrowser: true,
-        capabilities: {
-          browserName: 'chrome',
-          resolution: '1024x768',
-          args: [
-            '--headless',
-            '--foo',
-            '--bar'
-          ]
-        }
-      });
-      should.equal(
-				driverCore.constructor.name,
-				'thenableWebDriverProxy',
-				'library should be a thenableWebDriverProxy function'
-            );
-      done();
-    });
-  });
+//   describe('returns expected configured driver library', () => {
+//     it('validate the driver core constructor name', (done) => {
+//       driverCore = innerDriverCore({
+//         useBrowser: true,
+//         capabilities: {
+//           browserName: 'chrome',
+//           resolution: '1024x768',
+//           args: [
+//             '--headless',
+//             '--foo',
+//             '--bar'
+//           ]
+//         }
+//       });
+//       should.equal(
+// 				driverCore.constructor.name,
+// 				'thenableWebDriverProxy',
+// 				'library should be a thenableWebDriverProxy function'
+//             );
+//       done();
+//     });
+//   });
 
-describe('framework is object with useBrowser framework prop is used', () => {
-    it ('triggers the local browser on passing useBrowser attributes for chrome', (done) => {
-      driverCore = innerDriverCore({
-        useBrowser: true,
-        capabilities: {
-          browserName: 'chrome',
-          resolution: '1024x768',
-          args: [
-            '--headless',
-            '--foo',
-            '--bar'
-          ]
-        }
-      });
-      should.equal(
-  			driverCore.framework.useBrowser,
-  			true,
-  			'library should be configured for chrome browser'
-          );
-       done();
-    })
-    it ('triggers the local browser on passing useBrowser attributes for firefox', (done) => {
-        driverCore = innerDriverCore({
-          useBrowser: true,
-          capabilities: {
-            browserName: 'firefox',
-            resolution: '1024x768',
-            alwaysMatch: {
-                'moz:firefoxOptions': {
-                  args: [
-                    '--headless',
-                    '--foo',
-                    '--bar'
-                  ]
-                }
-              }
-          }
-        });
-        should.equal(
-                driverCore.framework.useBrowser,
-                true,
-                'library should be configured for chrome browser'
-            );
-        done();
-      })
-  })
+// describe('framework is object with useBrowser framework prop is used', () => {
+//     it ('triggers the local browser on passing useBrowser attributes for chrome', (done) => {
+//       driverCore = innerDriverCore({
+//         useBrowser: true,
+//         capabilities: {
+//           browserName: 'chrome',
+//           resolution: '1024x768',
+//           args: [
+//             '--headless',
+//             '--foo',
+//             '--bar'
+//           ]
+//         }
+//       });
+//       should.equal(
+//   			driverCore.framework.useBrowser,
+//   			true,
+//   			'library should be configured for chrome browser'
+//           );
+//        done();
+//     })
+//     it ('triggers the local browser on passing useBrowser attributes for firefox', (done) => {
+//         driverCore = innerDriverCore({
+//           useBrowser: true,
+//           capabilities: {
+//             browserName: 'firefox',
+//             resolution: '1024x768',
+//             alwaysMatch: {
+//                 'moz:firefoxOptions': {
+//                   args: [
+//                     '--headless',
+//                     '--foo',
+//                     '--bar'
+//                   ]
+//                 }
+//               }
+//           }
+//         });
+//         should.equal(
+//                 driverCore.framework.useBrowser,
+//                 true,
+//                 'library should be configured for chrome browser'
+//             );
+//         done();
+//       })
+//   })
   
-});
+// });
